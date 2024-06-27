1a35ef6288714c0823c42939d912df863c82c1e0
Faisal Shahzad <faisalshahzadyt@gmail.com>
2024-06-27 13:18:39 +0500

    added update modal and func

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,4 @@
+let allUsers = [];
 const verifyAccessToken = async (token) => {
   try {
     const req = await fetch(`/verify`, {
@@ -36,26 +37,28 @@ const getUsers = async (token) => {
   }
 };
 
-const insetUser = async ({ name, username, email, accessToken: token }) => {
-  // console.log(`Name is : ${name}`);
-  // console.log(`Username is : ${username}`);
-  // console.log(`Email is : ${email}`);
-  // console.log(`Token is : ${token}`);
+const insertUser = async ({ name, username, email, accessToken: token }) => {
+  try {
+    const req = await fetch(`/addUser`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ name, username, email }),
+    });
 
-  const req = await fetch(`/addUser`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({ name, username, email }),
-  });
+    const insertUserResponse = await req.json();
 
-  // if (!req.ok) {
-  //   return { success: false, message:  `Failed To Add User!` };
-  // }
-
-  return await req.json();
+    if (insertUserResponse.success) {
+      return { success: true };
+    } else {
+      return { success: false, message: insertUserResponse.message };
+    }
+  } catch (error) {
+    console.log(error);
+    return { success: false, message: "Failed to add user" };
+  }
 };
 
 const renderUsers = (users) => {
@@ -64,12 +67,12 @@ const renderUsers = (users) => {
 
   if (users.length === 0) {
     usersTable.innerHTML = `<tr>
-    <td>No User Found</td>
-    <td>No User Found</td>
-    <td>No User Found</td>
-    <td>No User Found</td>
-    <td>No User Found</td>
-    <td>No User Found</td>
+      <td>No User Found</td>
+      <td>No User Found</td>
+      <td>No User Found</td>
+      <td>No User Found</td>
+      <td>No User Found</td>
+      <td>No User Found</td>
     <tr>`;
     return;
   }
@@ -92,101 +95,213 @@ const renderUsers = (users) => {
     let created_at_td = document.createElement("td");
     created_at_td.textContent = user?.created_at;
 
-    let editButton = document.createElement("button");
-    editButton.classList.add("btn");
-    editButton.classList.add("btn-sm");
-    editButton.classList.add("btn-outline-primary");
-    editButton.classList.add("my-1");
-    editButton.classList.add("w-100");
-    editButton.setAttribute("data-user-id", user.id);
-    editButton.textContent = "Edit";
+    let buttons_td = document.createElement("td");
 
-    editButton.addEventListener("click", (e) => {
-      e.preventDefault();
+    let editButton = createButton("Edit", "btn-outline-primary", () =>
+      updateUser(user)
+    );
 
-      let user_data_id = e.target.getAttribute("data-user-id");
+    let deleteButton = createButton("Delete", "btn-outline-danger", () =>
+      deleteUser(user)
+    );
 
-      if (!user_data_id) return console.warn(`User Not Found!`);
-      const user = users.find((user) => user.id == user_data_id);
-      if (!user) return console.warn(`User Not Found!`);
-
-      editUser(user).then(() => {
-        console.log(`User Edited successfully!`);
-      });
-    });
-
-    let deleteButton = document.createElement("button");
-    deleteButton.classList.add("btn");
-    deleteButton.classList.add("btn-sm");
-    deleteButton.setAttribute("data-user-id", user.id);
-    deleteButton.classList.add("btn-outline-danger");
-    deleteButton.classList.add("my-1");
-    deleteButton.classList.add("w-100");
-    deleteButton.textContent = "Delete";
-
-    deleteButton.addEventListener("click", (e) => {
-      e.preventDefault();
-
-      let user_data_id = e.target.getAttribute("data-user-id");
-
-      if (!user_data_id) return console.warn(`User Not Found!`);
-      const user = users.find((user) => user.id == user_data_id);
-      if (!user) return console.warn(`User Not Found!`);
-
-      deleteUser(user).then(() => {
-        console.log(`User Deleted successfully!`);
-      });
-    });
+    buttons_td.appendChild(editButton);
+    buttons_td.appendChild(deleteButton);
 
     row.appendChild(id_td);
     row.appendChild(name_td);
     row.appendChild(username_td);
     row.appendChild(email_td);
     row.appendChild(created_at_td);
-    row.appendChild(editButton);
-    row.appendChild(deleteButton);
-
-    let buttonTd = document.createElement("td");
-    buttonTd.appendChild(editButton);
-    buttonTd.appendChild(deleteButton);
-
-    row.appendChild(buttonTd);
+    row.appendChild(buttons_td);
 
     usersTable.appendChild(row);
   });
 };
 
-const updateUsersList = async (allUsers) => {
-  let accessToken = window.location.search.split("?")[1].split("=")[1];
+const updateUser = async (user) => {
+  try {
+    console.log(user);
+    if (!user) {
+      return console.warn(`Invalid user!`);
+    }
 
-  if (!accessToken) window.location.href = "/";
+    const nameInput = document.getElementById("updateUser_name");
+    const usernameInput = document.getElementById("updateUser_username");
+    const emailInput = document.getElementById("updateUser_email");
+    const updateButton = document.getElementById("updateUser_button");
 
-  const usersResponse = await getUsers(accessToken);
-  console.log(usersResponse);
-  if (!usersResponse.success) {
-    return (window.location.href = "/");
+    nameInput.value = user?.name;
+    usernameInput.value = user?.username;
+    emailInput.value = user?.email;
+
+    // Function to enable update button
+    const enableUpdateButton = () => {
+      updateButton.removeAttribute("disabled");
+    };
+    const disableUpdateButton = () => {
+      updateButton.setAttribute("disabled", true);
+    };
+
+    // Event listeners on input fields
+    nameInput.addEventListener("input", () => {
+      if (
+        nameInput.value !== user.name ||
+        usernameInput.value !== user.username ||
+        emailInput.value !== user.email
+      ) {
+        enableUpdateButton();
+      } else {
+        disableUpdateButton();
+      }
+    });
+    usernameInput.addEventListener("input", () => {
+      if (
+        nameInput.value !== user.name ||
+        usernameInput.value !== user.username ||
+        emailInput.value !== user.email
+      ) {
+        enableUpdateButton();
+      } else {
+        disableUpdateButton();
+      }
+    });
+    emailInput.addEventListener("input", () => {
+      if (
+        nameInput.value !== user.name ||
+        usernameInput.value !== user.username ||
+        emailInput.value !== user.email
+      ) {
+        enableUpdateButton();
+      } else {
+        disableUpdateButton();
+      }
+    });
+
+    const updateUserModal = new bootstrap.Modal("#updateUserModal");
+    updateUserModal.show();
+  } catch (error) {
+    console.log(`An Error Occured: ${error}`);
   }
-  if (usersResponse.users) {
-    allUsers = usersResponse?.users;
-    renderUsers(usersResponse?.users);
-  }
-};
-
-const editUser = async (user) => {
-  console.log(user);
 };
 
 const deleteUser = async (user) => {
   console.log(user);
+  // Implement delete logic here
+};
+
+const openAddUserModal = () => {
+  let addUserModal = new bootstrap.Modal("#addUserModal");
+  addUserModal.show();
+};
+
+const handleAddUser = async (accessToken) => {
+  const spinner = `<div class="spinner-border spinner-border-sm" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>`;
+
+  const insertUserButton = document.getElementById("addUser_button");
+  const errorDiv = document.getElementById("addUserModalErrorDiv");
+
+  insertUserButton.disabled = true;
+  insertUserButton.innerHTML = spinner;
+
+  try {
+    let name = document.getElementById("newUser_name").value.trim();
+    let username = document.getElementById("newUser_username").value.trim();
+    let email = document.getElementById("newUser_email").value.trim();
+
+    if (!name || !username || !email) {
+      throw new Error("Please fill all the fields");
+    }
+
+    const insertUserResponse = await insertUser({
+      name,
+      username,
+      email,
+      accessToken,
+    });
+
+    if (!insertUserResponse.success) {
+      throw new Error(insertUserResponse.message);
+    }
+
+    errorDiv.textContent = "User Inserted Successfully!";
+    errorDiv.classList.remove("opacity-0");
+    errorDiv.classList.remove("bg-danger");
+    errorDiv.classList.add("bg-info");
+
+    setTimeout(() => {
+      errorDiv.classList.add("opacity-0");
+      errorDiv.classList.remove("bg-info");
+      document.getElementById("closeAddUserModalBtn").click();
+    }, 2000);
+
+    await updateUsersList();
+  } catch (error) {
+    console.log(`An Error Occured: ${error.message}`);
+    errorDiv.textContent = error.message;
+    errorDiv.classList.remove("opacity-0");
+    setTimeout(() => {
+      errorDiv.classList.add("opacity-0");
+    }, 2500);
+  } finally {
+    insertUserButton.disabled = false;
+    insertUserButton.innerHTML = "Add User";
+  }
+};
+
+const updateUsersList = async () => {
+  let accessToken = window.location.search.split("?")[1].split("=")[1];
+
+  if (!accessToken) window.location.href = "/";
+
+  try {
+    const verifyAccessTokenResponse = await verifyAccessToken(accessToken);
+
+    if (!verifyAccessTokenResponse.success) {
+      return (window.location.href = "/");
+    }
+
+    const getUsersResponse = await getUsers(accessToken);
+
+    if (!getUsersResponse.success) {
+      return (window.location.href = "/");
+    }
+
+    const { users } = getUsersResponse;
+    allUsers = users;
+    renderUsers(allUsers);
+  } catch (error) {
+    console.log("Error updating user list:", error);
+    window.location.href = "/";
+  }
 };
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const searchBox = document.getElementById("searchBox");
-  const spinner = `<div class="spinner-border spinner-border-sm" role="status">
-  <span class="visually-hidden">Loading...</span>
-</div>`;
+  let accessToken = window.location.search.split("?")[1].split("=")[1];
 
-  let allUsers;
+  if (!accessToken) window.location.href = "/";
+
+  const verifyAccessTokenResponse = await verifyAccessToken(accessToken);
+
+  if (!verifyAccessTokenResponse.success) {
+    return (window.location.href = "/");
+  }
+
+  document.getElementById("loadingSpinner").classList.remove("active");
+
+  const getUsersResponse = await getUsers(accessToken);
+
+  if (!getUsersResponse.success) {
+    return (window.location.href = "/");
+  }
+
+  allUsers = getUsersResponse.users; // Store users in a variable accessible to search
+
+  renderUsers(allUsers);
+
+  const searchBox = document.getElementById("searchBox");
   searchBox.addEventListener("input", (e) => {
     let searchTerm = e.target.value.toLowerCase();
     const filteredUsers = allUsers.filter((user) => {
@@ -200,100 +315,17 @@ document.addEventListener("DOMContentLoaded", async () => {
     renderUsers(filteredUsers);
   });
 
-  let accessToken = window.location.search.split("?")[1].split("=")[1];
+  const addUserModalButton = document.getElementById("addUserTrigger");
+  addUserModalButton.addEventListener("click", openAddUserModal);
 
-  if (!accessToken) window.location.href = "/";
-
-  const verifyAccessTokenResponse = await verifyAccessToken(accessToken);
-
-  if (!verifyAccessTokenResponse.success) {
-    return (window.location.href = "/");
-  }
-  document.getElementById("loadingSpinner").classList.remove("active");
-
-  const getUsersResponse = await getUsers(accessToken);
-
-  if (!getUsersResponse.success) {
-    return (window.location.href = "/");
-  }
-
-  const { success, users } = getUsersResponse;
-  allUsers = users;
-  renderUsers(users);
-
-  // display add user modal on button click
-  const addUserModalButton = document.getElementById("addButton");
-  addUserModalButton.addEventListener("click", (e) => {
-    let addUserModal = new bootstrap.Modal("#addUserModal");
-    addUserModal.show();
-  });
-
-  // code to insert user in database
   const insertUserButton = document.getElementById("addUser_button");
-  const errorDiv = document.getElementById("addUserModalErrorDiv");
-  insertUserButton.addEventListener("click", async (e) => {
-    try {
-      insertUserButton.disabled = true;
-      insertUserButton.innerHTML = spinner;
-
-      e.preventDefault();
-
-      let name = document.getElementById("newUser_name").value.trim();
-      let username = document.getElementById("newUser_username").value.trim();
-      let email = document.getElementById("newUser_email").value.trim();
-
-      if (!name || !username || !email) {
-        errorDiv.textContent = "Please fill all the fields";
-        if (errorDiv.classList.contains("opacity-0")) {
-          errorDiv.classList.remove("opacity-0");
-          setTimeout(() => {
-            errorDiv.classList.add("opacity-0");
-          }, 2500);
-        }
-        return;
-      }
-      name.value = "";
-      username.value = "";
-      email.value = "";
-
-      const insetUserResponse = await insetUser({
-        name,
-        username,
-        email,
-        accessToken,
-      });
-
-      console.log(insetUserResponse);
-
-      if (!insetUserResponse.success) {
-        errorDiv.textContent = insetUserResponse.message;
-        if (errorDiv.classList.contains("opacity-0")) {
-          errorDiv.classList.remove("opacity-0");
-          setTimeout(() => {
-            errorDiv.classList.add("opacity-0");
-          }, 2500);
-        }
-        return;
-      }
-
-      errorDiv.textContent = "User Inserted Successfully!";
-      if (errorDiv.classList.contains("opacity-0")) {
-        errorDiv.classList.remove("opacity-0");
-        errorDiv.classList.remove("bg-danger");
-        errorDiv.classList.add("bg-info");
-        setTimeout(() => {
-          errorDiv.classList.add("opacity-0");
-          errorDiv.classList.remove("bg-info");
-          document.getElementById("closeAddUserModalBtn").click();
-        }, 2000);
-      }
-
-      updateUsersList(allUsers).then(() => console.log(`Users List Updated!`));
-    } catch (error) {
-      console.log(`An Error Occured: ${error.message}`);
-    } finally {
-      insertUserButton.disabled = false;
-      insertUserButton.innerHTML = "Add User";
-    }
-  });
+  insertUserButton.addEventListener("click", () => handleAddUser(accessToken));
 });
+
+function createButton(text, classes, clickHandler) {
+  const button = document.createElement("button");
+  button.classList.add("btn", "btn-sm", classes, "my-1", "w-100");
+  button.textContent = text;
+  button.addEventListener("click", clickHandler);
+  return button;
+}
